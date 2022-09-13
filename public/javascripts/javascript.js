233572bf9e71628ce620fab4287cf941c31b75ee
MoistyDev <xaviermecole@hotmail.com>
2022-09-13 17:40:22 -0400

    Features :
    -Added Game Cycle (Win/Lose)
    -Added Refreshing of the game
    -Fixed erasing and writing in the different columns

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,11 +1,10 @@
 import { getFile } from "./wordlist.js";
- 
 
 const WORDS = await getFile();
 let selectedWord = chooseRandomWord();
 let currentCase = 1;
 let currentRow = 1;
-let isEndOfRow = false
+let wordDiscovered = false;
 
 function chooseRandomWord() {
     const randomNumber = Math.floor(Math.random() * WORDS.length);
@@ -14,18 +13,8 @@ function chooseRandomWord() {
 
 function cheatModeVerification() {
     const cheatCode = "robert";
-    if (prompt("Enter cheat code ") == "robert") {
+    if (prompt("Enter cheat code ") == cheatCode) {
         alert("Here is the selected word : " + (selectedWord));
-    }
-}
-
-function updateEndOfRow() {
-    for (let i = 5; i <= 30; i += 5) {
-        if (currentCase == i + 1) {
-            isEndOfRow = true;
-            return;
-        }
-        isEndOfRow = false;
     }
 }
 
@@ -33,102 +22,10 @@ function isLetter(str) {
     return str.length === 1 && str.match(/[a-z]/i);
 }
 
-function initOnScreenKBD() {
-    const KEYS = document.querySelectorAll("[data-kbd-key]");
-    for (const key of KEYS) {
-        if (key.textContent != "ENTER" || key.textContent != "DELETE") {
-            key.addEventListener("click", function(e) {
-                writeLetter(key.textContent);
-            });
-        }
-        if (key.textContent == "ENTER") {
-            key.addEventListener("click", verifyAnswer);
-        }
-        if (key.textContent == "DELETE") {
-            key.addEventListener("click", resetCase);
-        }
-    }
-}
-
-function writeLetter(letter) {
-    const gridCase = document.querySelector('[data-grid-case="' + currentCase + '"]');
-    if (currentCase < 1) {
-        currentCase = 1;
-    }
-    if (isLetter(letter) && !isEndOfRow) {
-        gridCase.innerText = letter;
-        ++currentCase;
-        updateEndOfRow();
-    }
-}
-
-function resetCase() {
-    --currentCase;
-    if (currentCase < 1) {
-        currentCase = 1;
-    }
-    const gridCase = document.querySelector('[data-grid-case="' + currentCase + '"]');
-    gridCase.innerHTML = "&nbsp";
-    updateEndOfRow();
-}
-
-function verifyAnswer() {
-    if (isEndOfRow) {
-        for (let i = 5; i <= 30; i += 5) {
-            console.log(i);
-            console.log(currentCase + "Current");
-            if (i - 1 == currentCase) {
-                console.log("test test");
-                for (let j = 0; j <= 5; j++) {
-                    const gridCase = document.querySelector('[data-grid-case="' + (i - j) + '"]');
-                    gridCase.setAttribute('data-case-to-check');
-                }
-                const letters = document.querySelectorAll('[data-case-to-check]');
-                const word = concat(letters);
-                if (word == selectedWord) {
-                    alert("GOOD !");
-                    return true;
-                }
-                if (word != selectedWord) {
-                    alert("Wrong !");
-                    return false;
-                }
-            }
-        }
-    }
-}
-
-function verifyLetter(gridCase, gridRow) {
-    let rowOfLetters = document.querySelectorAll('[data-grid-row ="' + gridRow + '"]');
-    const word = selectedWord.split('');
-    for (const letter of rowOfLetters) {
-        if ((letter.getAttribute('[data-grid-case]')) == gridCase) {
-            let selectedLetter = document.querySelector('[data-grid-case="' + gridCase + '"]');
-            for (let i = 0; i <= 5; i++) {
-                if (word[i] == valueOf(selectedLetter) && i == gridCase) {
-                    setAsDiscovered(gridCase);
-                } else if (word.includes(valueOf(selectedLetter))) {
-                    setAsInTheWord();
-                } else {
-                    
-                }
-            }
-        }
-    }
-}
-
-function setAsDiscovered(gridcase) {
-    let letter = document.querySelector('[data-grid-case="' + gridCase + '"]');
-}
-
-function setAsInTheWord() {
-
-}
-
 function readUserInputs() {
     let letter;
     document.addEventListener("keydown", function(e) {
-        if(window.event) {                  
+        if(window.event && !wordDiscovered) {                  
             letter = e.key;
             if (isLetter(letter)) {
                 writeLetter(letter.toUpperCase());
@@ -138,14 +35,185 @@ function readUserInputs() {
                 resetCase();
             }
             if (letter == "Enter") {
-                console.log();
                 verifyAnswer();
             }
         }
     });
 }
 
+function initOnScreenKBD() {
+    const KEYS = document.querySelectorAll("[data-kbd-key]");
+    for (const key of KEYS) {
+        if (key.textContent != "ENTER" || key.textContent != "DELETE" && !wordDiscovered) {
+            key.addEventListener("click", function(e) {
+                writeLetter(key.textContent);
+            });
+        }
+        if (key.textContent == "ENTER" && !wordDiscovered) {
+            key.addEventListener("click", verifyAnswer);
+        }
+        if (key.textContent == "DELETE" && !wordDiscovered) {
+            key.addEventListener("click", resetCase);
+        }
+    }
+}
+
+function writeLetter(letter) {
+    const gridCase = document.querySelector('[data-grid-case="' + currentCase + '"]');
+    const gridRow = document.querySelectorAll('[data-grid-row]');
+    if (currentCase < 1) {
+        currentCase = 1;
+    }
+    if (isLetter(letter) && gridRow[currentCase - 1].getAttribute('data-grid-row') == currentRow) {
+        gridCase.innerText = letter;
+        ++currentCase;
+    }
+}
+
+function resetCase() {
+    const gridCase = document.querySelector('[data-grid-case="' + (currentCase - 1) + '"]');
+    if (currentCase == 1) {
+        gridCase = document.querySelector('[data-grid-case="' + currentCase + '"]');
+    }
+    if (gridCase.getAttribute("data-grid-row") == currentRow) {
+        gridCase.innerHTML = "&nbsp";
+        --currentCase;
+        if (currentCase < 1) {
+            currentCase = 1;
+        }
+    }
+}
+
+function verifyAnswer() {
+    let rowOfLetters = document.querySelectorAll('[data-grid-row="' + currentRow + '"]');
+    let letterToCheck = currentCase;
+    let lettersMatching = 0;
+
+    if (rowOfLetters[0].textContent != "&nbsp" && !entryIsInList(rowOfLetters)) {
+        alert("Entry Unavailable.");
+    }
+    if (rowOfLetters[0].textContent != "&nbsp" && entryIsInList(rowOfLetters)) {
+        for (let i = 6; i > 0; i--){
+            lettersMatching += verifyLetter(letterToCheck, rowOfLetters, i);
+            letterToCheck--;
+        }
+        if(lettersMatching == rowOfLetters.length) {
+            wordIsDiscovered();
+        }
+        if (currentRow == 6 && !wordDiscovered) {
+            wordIsNotDiscovered();
+        }
+        incrementCurrentRow();
+    }
+}
+
+function incrementCurrentRow() {
+    for (let i = 5; i < 30; i += 5) {
+        if (currentCase == i + 1) {
+            currentRow++;
+        }
+    }
+}
+
+function entryIsInList(rowOfLetters) {
+    let word = "";
+    for (const letter of rowOfLetters) {
+        word += letter.textContent;
+    }
+    word = word.toLowerCase();
+    for (let i = 0; i < WORDS.length; i++) {
+        if (WORDS.includes(word)) {
+            return true;
+        }
+        return false;
+    }
+}
+
+function wordIsDiscovered() {
+    alert("Congratulations ! You discovered the word.");
+    wordDiscovered = true;
+}
+
+function wordIsNotDiscovered() {
+    alert("Nice Try ! The word was : " + selectedWord);
+}
+
+function verifyLetter(gridCase, rowOfLetters, index) {
+    const word = selectedWord.split('');
+    for (const letter of rowOfLetters) {
+        if ((letter.getAttribute('data-grid-case')) == gridCase) {
+            let selectedLetter = document.querySelector('[data-grid-case="' + gridCase + '"]');
+            if (selectedLetter.textContent == word[index - 1]) {
+                matchesWord(selectedLetter);
+                const keyboardKeys = document.querySelectorAll('[data-kbd-key]');
+                for (const key of keyboardKeys) {
+                    if (key.getAttribute('data-kbd-key').toUpperCase() == selectedLetter.textContent) {
+                        matchesWord(key);
+                    }
+                }
+                return 1;
+            } 
+            if (word.includes(selectedLetter.textContent)) {
+                inWord(selectedLetter);
+                const keyboardKeys = document.querySelectorAll('[data-kbd-key]');
+                for (const key of keyboardKeys) {
+                    if (key.getAttribute('data-kbd-key').toUpperCase() == selectedLetter.textContent) {
+                        inWord(key);
+                    }
+                }
+                return 0;
+            } 
+            if (word[index - 1] != selectedLetter.textContent && !word.includes(selectedLetter.textContent)) {
+                notInWord(selectedLetter);
+                const keyboardKeys = document.querySelectorAll('[data-kbd-key]');
+                for (const key of keyboardKeys) {
+                    if (key.getAttribute('data-kbd-key').toUpperCase() == selectedLetter.textContent) {
+                        notInWord(key);
+                    }
+                }
+                return 0;
+            }
+        }
+    }
+    return 0;
+}
+
+function notInWord(selectedLetter) {
+    selectedLetter.classList.add("notInTheWord");
+}
+
+function inWord(selectedLetter) {
+    selectedLetter.classList.add("isInTheWord");
+}
+
+function matchesWord(selectedLetter) {
+    selectedLetter.classList.add("matches");
+}
+
+function refreshGrid() {
+    const refreshButton = document.querySelector('[data-refresh-icon]');
+    refreshButton.addEventListener("click", function(e) {
+        const gridCases = document.querySelectorAll('[data-grid-case]');
+        for (const gridCase of gridCases) {
+            gridCase.innerHTML = "&nbsp";
+            gridCase.classList.remove("matches");
+            gridCase.classList.remove("isInTheWord");
+            gridCase.classList.remove("notInTheWord");
+        }
+        const keyboardKeys = document.querySelectorAll('[data-kbd-key]');
+        for (const key of keyboardKeys) {  
+            key.classList.remove("matches");
+            key.classList.remove("isInTheWord");
+            key.classList.remove("notInTheWord");
+        }
+        selectedWord = chooseRandomWord();
+        currentCase = 1;
+        currentRow = 1;
+        wordDiscovered = false;
+    });
+}
+
 initOnScreenKBD();
 cheatModeVerification();
 readUserInputs();
-console.log(WORDS);
+refreshGrid();
